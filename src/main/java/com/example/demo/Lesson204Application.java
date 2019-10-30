fb4e5e61f20780842c25bdf8adb4dcfd4b129407
rcorcios023 <56006220+rcorcios023@users.noreply.github.com>
2019-10-29 21:47:19 -0400

    lesson_204

---
+++ src/main/java/com/example/demo/Lesson204Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson204Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson204Application.class, args);
+    }
+
+}
